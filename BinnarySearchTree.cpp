6117a362edfdde6ac323ec6648d07068c856b5f6
rukarajinbelajar <maar1101dnd@gmail.com>
2026-06-12 15:39:50 +0700

    membuat dan mendefinisikan prosedur preorder

--- BinnarySearchTree.cpp
+++ BinnarySearchTree.cpp
@@ -76,5 +76,19 @@ public:
             inorder(ptr->rightChild);
         }
     }
+
+    void preorder(Node* ptr) {
+
+        if (ROOT == nullptr) {
+            cout << "Tree is empty" << endl;
+            return;
+
+        }
+        if (ptr !=nullptr) {
+                cout << ptr->info << " ";
+                preorder(ptr->leftChild);
+                preorder(ptr->rightChild);
+        }
+    }
     
 }
